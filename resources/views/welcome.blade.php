553776d8e195628aca217ab8ab76ba389956bd67
José Omar <joseomar_coppola@hotmail.com>
2025-05-03 19:37:02 +0200

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Herd Playground</title>
+</head>
+<body>
+    <h1>
+        Welcome to the Laravel Playground
+    </h1>
+        <p> Click the button below to get a list</p>
+        <a href = "" class="btn">
+            get your list!
+        </a>
+</body>
+</html>
