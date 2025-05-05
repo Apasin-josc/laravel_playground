34b3236589a78c4ec9fbe34d6a3d4f5d534ea0ce
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-05 14:38:55 +0200

    learning a little more about eloquent models, php artisan tinker op

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'priority',
+        'description',
+    ];
+
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+}
