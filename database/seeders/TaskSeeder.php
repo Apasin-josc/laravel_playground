09a0ad219aa9144212d003139848377985d66813
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-05 15:07:23 +0200

    adding to the project a taskseeder file, learning how to drop the tables and populate them using the seeder files

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Task;
 
 class TaskSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Task::factory()
+            ->count(50)
+            ->create();
     }
 }
