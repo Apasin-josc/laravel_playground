5facb8cae17e5ff30709f4a05294092365c8ba0b
omarscoppola <joseomar_coppola@hotmail.com>
2025-05-07 09:04:35 +0200

    registering new users to the application, maybe investigate on a function a verification email function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -16,13 +18,23 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function register()
+    public function register(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
 
+        $user = User::create($validated);
+        
+        Auth::login($user);
+
+        return redirect()->route('tasks.index')->with('success', 'Registration successful. Please log in.');
     }
 
     public function login()
     {
-        
+
     }
 }
